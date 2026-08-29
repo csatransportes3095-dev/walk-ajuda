c0f4a01f73904118bc7f5e004fd832161ca10962
csatransportes3095-dev <csatransportes3095@gmail.com>
2026-08-29 02:19:01 -0300

    feat(admin-products): enable question tree UX enhancer

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import { httpBatchLink, splitLink, TRPCClientError } from "@trpc/client";
 import { createRoot } from "react-dom/client";
 import superjson from "superjson";
 import App from "./App";
+import AdminProductsQuestionUXEnhancer from "./components/AdminProductsQuestionUXEnhancer";
 import "./index.css";
 
 // Consultas de tela não podem ficar em loop por vários minutos quando o servidor
@@ -86,6 +87,7 @@ const trpcClient = trpc.createClient({
 createRoot(document.getElementById("root")!).render(
   <trpc.Provider client={trpcClient} queryClient={queryClient}>
     <QueryClientProvider client={queryClient}>
+      <AdminProductsQuestionUXEnhancer />
       <App />
     </QueryClientProvider>
   </trpc.Provider>
